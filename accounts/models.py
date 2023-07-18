688f44e179640b794b86e16647bcd8c3356b8cbc
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-07-18 21:04:47 +0100

    add model of product_favorite for users

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from allproducts.models import product
 # Create your models here.
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
+    product_favorite = models.ManyToManyField(product)
 
     address = models.CharField(max_length=60)
     address2 = models.CharField(max_length=60)
