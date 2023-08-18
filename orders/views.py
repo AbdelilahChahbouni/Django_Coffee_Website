dbe0c27e9e3afc1a60ee32a7887ca8317b8a625f
abdelilah <local-host@gmail.com>
2023-08-18 16:37:04 +0100

    build logic of add_to_cart function in views part one

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render , redirect
 # Create your views here.
 
 def add_to_cart(request):
-    return redirect("products")
+    return redirect("allproducts")
+
+
+
+
 
 
 
